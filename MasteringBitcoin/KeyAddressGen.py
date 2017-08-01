9f665fc91d398342f2f78bf40bb800693d9b4702
yenchihliao <jeff.yenchih.liao@gmail.com>
2017-08-01 11:42:49 +0800

    finished all processes of generating keys and addresses

--- MasteringBitcoin/KeyAddressGen.py
+++ MasteringBitcoin/KeyAddressGen.py
@@ -1,7 +1,7 @@
 """ 
 #this is python demo code for keys and Addresses generation
 #copy form mastering bitcoin page 81, 82
-#importing pybitcointools instead of bitcoin
+#importing pybitcointools instead of bitcoin which shown on the book
 """
 
 from pybitcointools import *
@@ -31,7 +31,29 @@ wif_compressed_private_key = encode_privkey(
 print "Private Key (WIF-Compressed) is: ", wif_compressed_private_key
 
 #Multiply the EC generator point G with the private key to get a public key point
-#base10 multiply not found, fast_multiply used instead
+#base10 multiply(which is used on the book) not found, fast_multiply used instead
 public_key = fast_multiply(G, decoded_private_key)
 print "Public Key (x, y) coordinate is: ", public_key
+
+#encode as hex, prefix 04 (uncompressed Bitcoin Address)
+hex_encoded_public_key = encode_pubkey(public_key, 'hex')
+print "Public Key (hex) is: ", hex_encoded_public_key
+
+#compress public key, adjust prefix depending on whether y is even or odd(comporessed pubkey)
+(public_key_x, public_key_y) = public_key
+if (public_key_y % 2) == 0 :
+    compressed_prefix = '02'
+else:
+    compressed_prefix = '03'
+hex_compressed_public_key = compressed_prefix + encode(public_key_x, 16)
+print "Compressed Public Key (hex) is: ", hex_compressed_public_key
+
+#Generate bitcoin address from pubkey
+print "bitcoin Address (b58check) is: ", pubkey_to_address(public_key)
+
+#Generate bitcoin address from compressed public key
+print "Compressed bitcoin Address (b58check) is: ", pubkey_to_address(hex_compressed_public_key)
+
+
+
 
